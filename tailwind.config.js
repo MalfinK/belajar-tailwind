2c6e6b1ff096bcab24e2cc84d8c4bfd7b7a13211
MalfinK <muhammadalfin1235678@gmail.com>
2024-03-07 12:46:20 +0700

    vid3 tailwind css wpu kelar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
+// content: ['./public/**/*.{html,js}', './index.html'],
+/* 
+  Note 
+  - Arti dari baris yang dikomen adalah masuk ke root -> public -> apapun folder didalamnya -> apapun file yang ada di dalamnya yang berekstensi .html atau .js. Koma (,) disini berguna untuk melihat atau menambahkan file yang lainnya. Kenapa gaperlu ditambahkan index.html nya karena file tersebut sudah berada di dalam folder public
+*/
