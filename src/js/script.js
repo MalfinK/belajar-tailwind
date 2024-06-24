aeaf9d6380c2e60c47b26052d9933066a5d6dab5
MalfinK <muhammadalfin1235678@gmail.com>
2024-06-24 22:39:18 +0700

    vid13 tailwind css wpu kelar

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+// Navbar fixed
+window.onscroll = function() { // saat window di-scroll
+    const header = document.querySelector('header');
+    // Cara 1 (Sendiri)
+    /* if (window.scrollY > 0) { // jika window di-scroll lebih dari 0
+        header.classList.add('navbar-fixed'); // tambahkan class navbar-fixed
+    } else { // jika tidak
+        header.classList.remove('navbar-fixed'); // hapus class navbar-fixed
+    } */
+
+    // Cara 2 (Dari WPU)
+    const fixedNav = header.offsetTop; // mendapatkan jarak dari atas ke header
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
+// Untuk bagian hamburger menu
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+/*  
+Note:
+Bisa add (menambahkan) class baru, remove (menghapus) class yang sudah ada, dan toggle (menambahkan jika belum ada, menghapus jika sudah ada) class yang sudah ada.
+*/
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        'primary': '#2dd4bf',
+        'secondary': '#64748b',
+        'dark': '#0f766e',// aturan pake text-slate-900 tapi ini pake text-teal-700
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
+/*  
+Note:
+Kalau extend kan menambahkan dari fitur yang sudah dimiliki oleh tailwindcss, tapi kalau langsung di dalam theme, maka akan menggantikan fitur yang sudah ada (menimpa) di tailwindcss tersebut.
+*/
