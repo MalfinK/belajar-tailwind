ac04b1b64637ffb3666258d2689c8cd3e205decf
MalfinK <muhammadalfin1235678@gmail.com>
2024-06-27 20:28:38 +0700

    prelakuan prettier di file script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,29 @@
 // Navbar fixed
-window.onscroll = function() { // saat window di-scroll
-    const header = document.querySelector('header');
-    // Cara 1 (Sendiri)
-    /* if (window.scrollY > 0) { // jika window di-scroll lebih dari 0
+window.onscroll = function () {
+  // saat window di-scroll
+  const header = document.querySelector("header");
+  // Cara 1 (Sendiri)
+  /* if (window.scrollY > 0) { // jika window di-scroll lebih dari 0
         header.classList.add('navbar-fixed'); // tambahkan class navbar-fixed
     } else { // jika tidak
         header.classList.remove('navbar-fixed'); // hapus class navbar-fixed
     } */
 
-    // Cara 2 (Dari WPU)
-    const fixedNav = header.offsetTop; // mendapatkan jarak dari atas ke header
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
+  // Cara 2 (Dari WPU)
+  const fixedNav = header.offsetTop; // mendapatkan jarak dari atas ke header
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
 };
 
 // Untuk bagian hamburger menu
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 /*  
